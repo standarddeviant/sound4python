5a2ba613dfaf2cf76f76a7354343158abdf45b00
standarddeviant <dave.crist@gmail.com>
2013-03-10 10:12:04 -0400

    fixing minor bugs

--- sound4python.py
+++ sound4python.py
@@ -33,54 +33,57 @@ def sound(itr,samprate=16000,autoscale=True,output=False):
     #create file in memory
     #with tempfile.SpooledTemporaryFile() as memFile:
     memFile = tempfile.SpooledTemporaryFile()
-    if(True):
-        waveWrite = wave.open(memFile,'wb')
-        waveWrite.setsampwidth(2)        # int16 default
-        waveWrite.setnchannels(1)        # mono  default
-        waveWrite.setframerate(samprate) # 8kHz  default
-        
-        wroteFrames=False
-        if( foundNumpy ):
-            if( type(itr)==np.array ):
-                if( itr.ndim == 1 or itr.shape.count(1) == itr.ndim - 1 ):
-                    waveWrite.writeframes( (mult*itr.flatten()).astype(np.int16).tostring() )
-                    wroteFrames=True
-            else: #we have np, but the iterable isn't a vector
-                waveWrite.writeframes( (mult*np.array(itr)).astype(np.int16).tostring() )
+
+    #create wave write objection pointing to memFile
+    waveWrite = wave.open(memFile,'wb')
+    waveWrite.setsampwidth(2)        # int16 default
+    waveWrite.setnchannels(1)        # mono  default
+    waveWrite.setframerate(samprate) # 8kHz  default
+    
+    wroteFrames=False
+    #Let's try to create sound from NumPy vector
+    if( foundNumpy ):
+        if( type(itr)==np.array ):
+            if( itr.ndim == 1 or itr.shape.count(1) == itr.ndim - 1 ):
+                waveWrite.writeframes( (mult*itr.flatten()).astype(np.int16).tostring() )
                 wroteFrames=True
-        if( not wroteFrames and not foundNumpy ):
-            #python w/o np doesn't have short/int16, "@h" is "native,aligned short"
-            waveWrite.writeframes( struct.pack(len(itr)*"@h",[int(mult*itm) for  itm in itr]) )
+        else: #we have np, but the iterable isn't a vector
+            waveWrite.writeframes( (mult*np.array(itr)).astype(np.int16).tostring() )
             wroteFrames=True
-            
-        if( not wroteFrames ):
-            print("E: Unable to create sound.  Only 1D numpy arrays and numerical lists are supported.")
-            return None
+    if( not wroteFrames and not foundNumpy ):
+        #python w/o np doesn't have "short"/"int16", "@h" is "native,aligned short"
+        waveWrite.writeframes( struct.pack(len(itr)*"@h",[int(mult*itm) for  itm in itr]) )
+        wroteFrames=True
+        
+    if( not wroteFrames ):
+        print("E: Unable to create sound.  Only 1D numpy arrays and numerical lists are supported.")
+        waveWrite.close()
+        return None
 
-        memFile.seek(0) #configure the file object, memFile, as if we're just starting to read it
+    #configure the file object, memFile, as if it has just been opened for reading
+    memFile.seek(0)
 
-        try:
-            # getting here means wroteFrames == True
-            print("\nAttempting to play a mono audio stream of length")
-            print("  %.2f seconds (%.3f thousand samples at sample rate of %.3f kHz)"%
-                    ( 1.0*len(itr)/samprate , len(itr)/1000. , int(samprate)/1000.) )
-            p=launchWithoutConsole(['sox','-','-d'])
-        except: 
-            print("E: Unable to launch sox.")
-            print("E: Please ensure that sox is installed and on the path.")
-            print("E: Try 'sox -h' to test sox installation.")
-            waveWrite.close()
-            return None
+    try:
+        # getting here means wroteFrames == True
+        print("\nAttempting to play a mono audio stream of length")
+        print("  %.2f seconds (%.3f thousand samples at sample rate of %.3f kHz)"%
+                ( 1.0*len(itr)/samprate , len(itr)/1000. , int(samprate)/1000.) )
+        p=launchWithoutConsole(['sox','-','-d'])
+    except: 
+        print("E: Unable to launch sox.")
+        print("E: Please ensure that sox is installed and on the path.")
+        print("E: Try 'sox -h' to test sox installation.")
+        waveWrite.close()
+        return None
 
-        try: 
-            p.communicate(memFile.read())
-            p.wait()
-        except: 
-            print("E: Unable to send in-memory wave file to stdin of sox subprocess.")
-            waveWrite.close()
-            return None
-        #os.kill(p.pid,signal.CTRL_C_EVENT)
-    #print memFile.closed #deletes temporary file in RAM
+    try: 
+        p.communicate(memFile.read())
+        p.wait()
+    except: 
+        print("E: Unable to send in-memory wave file to stdin of sox subprocess.")
+        waveWrite.close()
+        return None
+    #os.kill(p.pid,signal.CTRL_C_EVENT)
 #end def sound(itr,samprate=8000,autoscale=True)
         
 
